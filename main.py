5053de40e3df8866ea67a9d7e147c4df56789ab9
vgire <vincent.gire@gmail.com>
2025-03-21 16:59:46 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+```python
+import math
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def primes(max=10):
+    primes = []
+    num = 2
+    while len(primes) <= max:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+print(primes(10))
+```

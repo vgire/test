d37b14becf589eb20ed84d674cd8cd2e3cc505f6
vgire <vincent.gire@gmail.com>
2025-03-25 14:56:30 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+function isPrime(n: number): boolean {
+  if (n < 2) {
+      return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+          return false;
+      }
+  }
+  return true;
+}
+
+function primes(max: number = 10): number[] {
+  const primes: number[] = [];
+  let num = 2;
+  while (primes.length <= max) {
+      if (isPrime(num)) {
+          primes.push(num);
+      }
+      num++;
+  }
+  return primes;
+}
+
+console.log(primes(10));

364dec01726e46899936cb65ed364bf7eb67f7b3
blist <hello@blist.dev>
2025-04-08 14:19:51 +0000

    Fixed issue 100

---
+++ src/index.test.ts
@@ -0,0 +1,14 @@
+import { primes } from './index';
+
+describe('primes', () => {
+    it('should return at most max results', () => {
+        expect(primes(10).length).toBe(10);
+        expect(primes(5).length).toBe(5);
+        expect(primes(1).length).toBe(1);
+    });
+
+    it('should return correct prime numbers', () => {
+        expect(primes(5)).toEqual([2, 3, 5, 7, 11]);
+        expect(primes(1)).toEqual([2]);
+    });
+});
